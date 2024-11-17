61118227f0d7a848f57c6cc184d36dbe8a14d30c
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2024-11-17 17:23:55 +0000

    adiciona scripts `lint:check`e lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnew.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function Home() {
       <h1>❤️Raysa, eu te amo! dá uma risadinha se vc gostou!❤️</h1>
       <button onClick={handleClick}>Clique aqui</button>
     </div>
-  )
+  );
 }
 
 export default Home;
