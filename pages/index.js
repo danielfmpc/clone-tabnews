b1683ff93816529b1faadb3d77b0f7cbc47762c8
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2024-11-17 01:31:43 +0000

    adcionando botao pega agua

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+const handleClick = () => {
+  alert("Pega agua pra mim, pf!");
+};
+
 function Home() {
-  return <h1>❤️Raysa, eu te amo, se você gostou da uma risadinha❤️</h1>
+  return (
+    <div>
+      <h1>❤️Raysa, eu te amo, se você gostou da uma risadinha❤️</h1>
+      <button onClick={handleClick}>Clique aqui</button>
+    </div>
+  )
 }
 
 export default Home;
