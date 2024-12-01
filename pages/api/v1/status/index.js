ea8c3ca0c4546b6a0f1bce65bc0cb64237f5aa18
Daniel Carlos <daniel_fmpc@hotmail.com.br>
2024-12-01 03:46:11 +0000

    finish api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-
-  console.log("result :>> ", result.rows);
-
-  response.status(200).json({
-    chave: "valor",
+  const result = await database.query({
+    text: `
+      SELECT 
+      current_setting('max_connections')::int as maxConnections, 
+      version() as versionDb, 
+      count(pid)::int as currentConnections 
+      from pg_stat_activity 
+      WHERE datname = $1
+    `,
+    values: [process.env.POSTGRES_DB],
   });
+
+  const updateAt = new Date().toISOString();
+
+  const status = {
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version_db: result.rows[0].versiondb,
+        max_connections: result.rows[0].maxconnections,
+        current_connections: result.rows[0].currentconnections,
+      },
+    },
+  };
+
+  response.status(200).json(status);
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,26 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const pasedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(pasedUpdatedAt);
+
+  const { version_db, max_connections, current_connections } =
+    responseBody.dependencies.database;
+
+  expect(version_db).toBeDefined();
+  expect(version_db).toBe(
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit",
+  );
+
+  expect(max_connections).toBeDefined();
+  expect(Number.isInteger(max_connections)).toBe(true);
+  expect(max_connections).toBeGreaterThanOrEqual(0);
+
+  expect(current_connections).toBeDefined();
+  expect(Number.isInteger(current_connections)).toBe(true);
+  expect(current_connections).toEqual(1);
 });
